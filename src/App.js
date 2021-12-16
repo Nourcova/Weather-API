eee8e881904f6d94461bef1c57436698e54d6964
RimaMohamadAssaad <rimaassaad1998kor@gmail.com>
2021-12-16 17:00:53 +0200

    first html push for team

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ import SayHi, { SayHello } from "./components/WeatherItem";
 import fakeWeatherData from "./fakeWeatherData.json";
 
 import "./App.css";
+import storm from "./img/weather-icons/storm.svg";
+import clear from "./img/weather-icons/clear.svg";
+import cloudy from "./img/weather-icons/cloudy.svg";
+import drizzle from "./img/weather-icons/drizzle.svg";
+import fog from "./img/weather-icons/fog.svg";
+import mostlycloudy from "./img/weather-icons/mostlycloudy.svg";
+import partlycloudy from "./img/weather-icons/partlycloudy.svg";
+import rain from "./img/weather-icons/rain.svg";
+import snow from "./img/weather-icons/snow.svg";
+
 
 class App extends Component {
   constructor(props) {
@@ -21,10 +31,59 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <SayHi />
-        <SayHello color="black" name={this.state.name} />
-        <Search handleInput={this.handleInputChange} />
+        <header>
+          <div className="input">
+            <input type="search" name="search" placeholder="Search City" /></div>
+          <div><button>find weather</button></div>
+        </header>
+
+        <div className="main">
+          <div className="img1"><img src={mostlycloudy} alt="storm icon" />
+            <p>overcast cloud</p>
+            <p>temperature 10<span> ℃ </span> to 11 <span> ℃</span></p>
+            <p><span>humidity</span><span> 78% </span><span> pressure </span><span> 1008.8 </span></p>
+            </div>
+          <div className="container">
+            <div className="weath">
+              <p>3:00</p>
+              <img src={mostlycloudy} alt="storm icon" />
+              <p>8</p>
+            </div>
+            <div className="weath">
+              <p>3:00</p>
+              <img src={mostlycloudy} alt="storm icon" />
+              <p>8</p>
+            </div>
+            <div className="weath">
+              <p>3:00</p>
+              <img src={mostlycloudy} alt="storm icon" />
+              <p>8</p>
+            </div>
+            <div className="weath">
+              <p>3:00</p>
+              <img src={mostlycloudy} alt="storm icon" />
+              <p>8</p>
+            </div>
+            <div className="weath">
+              <p>3:00</p>
+              <img src={mostlycloudy} alt="storm icon" />
+              <p>8</p>
+            </div>
+            <div className="weath">
+              <p>3:00</p>
+              <img src={mostlycloudy} alt="storm icon" />
+              <p>8</p>
+            </div>
+            <div className="weath">
+              <p>3:00</p>
+              <img src={mostlycloudy} alt="storm icon" />
+              <p>8</p>
+            </div>
+          </div>
+        </div>
+
       </div>
+
     );
   }
 }
