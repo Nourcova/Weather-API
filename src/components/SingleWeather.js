2ab604576c232d241b5aa13aae05dd80e55948ef
Nourcova <ibrahamnour22@gmail.com>
2021-12-18 23:57:56 +0200

    mapping the jason

--- src/components/SingleWeather.js
+++ src/components/SingleWeather.js
@@ -1,4 +1,13 @@
 import React, { Component } from 'react';
+import storm from "../img/weather-icons/storm.svg";
+import clear from "../img/weather-icons/clear.svg";
+import cloudy from "../img/weather-icons/cloudy.svg";
+import drizzle from "../img/weather-icons/drizzle.svg";
+import fog from "../img/weather-icons/fog.svg";
+import mostlycloudy from "../img/weather-icons/mostlycloudy.svg";
+import partlycloudy from "../img/weather-icons/partlycloudy.svg";
+import rain from "../img/weather-icons/rain.svg";
+import snow from "../img/weather-icons/snow.svg";
 
 class SingleWeather extends Component {
     render() {
